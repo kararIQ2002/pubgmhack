15d62f658c8727c9a1ba1d107c61557d033c4079
Bukandewa <mahadewanto2@gmail.com>
2018-09-06 20:37:55 +0700

    Add files via upload

--- main.lua
+++ main.lua
@@ -1,2 +1,2 @@
-gg.alert('download new loader in http://mahadewanto.online/download/file.php?id=14')
+gg.alert('download new loader in http://mahadewanto.online/download/file.php?id=15')
 gg.alert('thanks')
